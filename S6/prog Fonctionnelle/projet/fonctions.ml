8dadb4a5fd89bb0fb6e30bccb057166a38037e4d
BrinonBat <baptiste@brinon.eu>
2020-02-28 01:46:24 +0100

    liLock fait exactement le bon parcours, reste qu'a configurer resultat

--- S6/prog Fonctionnelle/projet/fonctions.ml
+++ S6/prog Fonctionnelle/projet/fonctions.ml
@@ -45,6 +45,7 @@ let rec successeurs graphe sommet =
 (*successeurs graphe1 3;;*)
 
 (*retourne le 2eme element de la liste*)
+(*val sec : 'a list list -> 'a list = <fun> *)
 let sec liste=
 	match liste with
 		[x;y;_]	-> y
@@ -61,11 +62,11 @@ let rec retirer l1 l2 =
 				else x::retirer r l2
 	| []   	-> []
 ;;
-
-
+(*retourne le sommet correspond au numero de sommet en paramètre*)
+(*val retourne_sommet : 'a -> ('a * 'b) list -> 'a * 'b = <fun> *)
 let rec retourne_sommet numSommet graphe=
 	match graphe with
-		((num,listSucc)::reste)	-> if (numSommet=num) then (num,listeSucc) else retourne_sommet numSommet reste
+		((num,listSucc)::reste)	-> if (numSommet=num) then (num,listSucc) else retourne_sommet numSommet reste
 		|_						-> failwith " sommet non existant dans le graphe"
 ;;
 (* PARCOURS PREFIXE
@@ -88,27 +89,14 @@ let parcours_profondeur graphe =
 	in parcours_interne (racine graphe) [] [numSommet (racine graphe)]
 ;;
 FIN PARCOURS PREFIXE *)
-
-let rec retourne_sommet numSommet graphe=
-	match graphe with
-		((num,listSucc)::reste)	-> if (numSommet=num) then (num,listSucc) else retourne_sommet numSommet reste
-		|_						-> failwith " sommet non existant dans le graphe"
-;;
-
 (*
 3 fonctions :
     1 parcours_profondeur graphe -> effectue un parcours en profondeur sur le graphe
     2 parcours_suffixe dejaTraite,pasEncoreVisite -> parcours suffixe tq tous les sommets de graphe ne sont pas traite
     3 traiter sommet,dejaTraite,graphe -> traite le sommet s'il n'est pas dans dejaTraite
 *)
-let rec traiter (num,succs) dejaTraite graphe =
-    if not(List.mem num dejaTraite)
-    then List.fold_left(fun resultat numSommet -> (traiter (retourne_sommet numSommet graphe) (num::resultat) graphe ))
-	                    dejaTraite
-	                    succs
-    else dejaTraite
-;;
-(*
+
+(* PARCOURS FACTORISE MAIS QUI COMPILE PAS
 let parcours_profondeur graphe =
 	let rec parcours_suffixe dejaTraite=
 	    (* on a fini une fois que tous les sommets ont été visités*)
@@ -132,11 +120,43 @@ let parcours_profondeur graphe =
 	in parcours_suffixe []
 ;;
 *)
-let parcours_profondeurBis graphe =
-	let parcours_suffixe dejaTraite=
-	   (*sinon, on traite la composante connexe suivante *)
-		List.fold_left (fun liDejaTraite sommet -> traiter liDejaTraite sommet graphe) List.hd(graphe) dejaTraite
-	in parcours_suffixe []
+(* AUTRE TENTATIVE , ON APPROCHE DU BUT
+(*effectue un parcours en profondeur suffixe à partir du sommet saisi en paramètre*)
+(*val traiter : 'a * 'a list -> 'a list -> ('a * 'a list) list -> 'a list = <fun>*)
+let rec traiter (num,succs) dejaTraite graphe =
+    if not(List.mem num dejaTraite)
+    then List.fold_left(fun resultat numSommet -> (traiter (retourne_sommet numSommet graphe) (num::resultat) graphe ))
+	                    dejaTraite
+	                    succs
+    else dejaTraite
 ;;
 
+(*val parcours_profondeurBis : ('a * 'a list) list -> 'a list = <fun>  *)
+let parcours_profondeurBis graphe =
+		List.fold_left (fun liDejaTraite sommet -> traiter sommet liDejaTraite graphe)
+		                []
+		                graphe
+;;
+*)
+
+(*effectue un parcours en profondeur suffixe à partir du sommet saisi en paramètre*)
+let rec traiter (num,succs) (liLock,resultat) graphe =
+    if not(List.mem num liLock)
+    then List.fold_left(fun (liLock,resultat) numSommet -> (traiter
+                                                        (retourne_sommet numSommet graphe)
+                                                        (liLock,(num::resultat))
+                                                        graphe)
+                        )
+	                    (num::liLock,resultat)
+	                    succs
+    else (liLock,resultat)
+;;
+
+let parcours_profondeur graphe =
+		List.fold_left (fun (liLock,resultat) sommet -> traiter sommet (liLock,resultat) graphe)
+		                ([],[])
+		                graphe
+;;
+
+parcours_profondeur graphe1;;
 (*parcours_profondeur graphe1 doit retourner - : int list = [2; 4; 3; 1; 8; 6; 7; 5]*)
