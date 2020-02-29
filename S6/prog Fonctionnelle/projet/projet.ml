6dc44a96d9f165718862d30858b58d2780e0cc5e
BrinonBat <baptiste@brinon.eu>
2020-02-29 17:50:07 +0100

    ajout code Chris Tarjan

--- S6/prog Fonctionnelle/projet/projet.ml
+++ S6/prog Fonctionnelle/projet/projet.ml
@@ -2,60 +2,132 @@ let graphe1 = [(1,[6;7;8]) ; (2,[1;4]) ; (3, [2]) ; (4, [3;5]) ; (5, [1])
 ; (6, [5;7]) ; (7, []) ; (8, [6;7])];;
 
 
-(retourne la liste des sommets dans graphe)
-let rec sommets graphe = match graphe with
-	((x,_)::r) -> x::(listeSommets r)
-	| [] -> [];;
-(*sommets graphe1;;*)
+(*fonction qui associe a chaque sommet le plus petit sommet accessible et un index*)
+(*ici -1 signifie non defini*)
+(*val init_lowlink : ('a * 'b) list -> ('a * int * int * bool) list = <fun> *)
+let rec init_lowlink graphe = match graphe with
+	(som, _)::reste -> (som, -1, -1, false) :: (init_lowlink reste)
+	| [] -> [];;		(*sommet, index, lowlink, onstack*)
+
+
+
+
+(*fonction qui prends la liste lowlink et retourne le plus grand index, pour savoir le plus petit indice qu'on utilise*)
+(* val unusedIndex : ('a * int * 'b * 'c) list -> int = <fun>  *)
+let rec unusedIndex lowlink =	match lowlink with
+	(_, index, _, _)::reste -> let unusedIndexReste = unusedIndex reste in if unusedIndexReste > index then unusedIndexReste else index
+	| [] -> -1;;
+
+
+(*accesseurs et mutateurs de la liste lowlink, les donnees qui sont necessaire pour la fonction tarjan*)
+(*val setIndex : ('a * 'b * 'c * 'd) list -> 'a -> 'b -> ('a * 'b * 'c * 'd) list = <fun> *)
+let rec setIndex lowlink sommet newIndex = match lowlink with
+	(som, index, link, onstack)::reste -> if som = sommet then (som, newIndex, link, onstack)::reste else (som, index, link, onstack):: (setIndex reste sommet newIndex)
+	| [] -> failwith "Sommet Invalide setIndex";;
+
+
+let rec setLink lowlink sommet newLink = match lowlink with
+	(som, index, link, onstack)::reste -> if som = sommet then (som, index, newLink, onstack)::reste else (som, index, link, onstack):: (setLink reste sommet newLink)
+	| [] -> failwith "Sommet Invalide setLink";;
+
+let rec setOnstack lowlink sommet newOnstack = match lowlink with
+	(som, index, link, onstack)::reste -> if som = sommet then (som, index, link, newOnstack)::reste else (som, index, link, onstack):: (setOnstack reste sommet newOnstack)
+	| [] -> failwith "Sommet Invalide setOnstack";;
+
+let rec getIndex lowlink sommet = match lowlink with
+	(som, index, link, onstack)::reste -> if som = sommet then index else getIndex reste sommet
+	| [] -> failwith "Sommet Invalide getIndex";;
+
+let rec getLink lowlink sommet = match lowlink with
+	(som, index, link, onstack)::reste -> if som = sommet then link else getLink reste sommet
+	| [] -> failwith "Sommet Invalide getLink";;
+
+let rec getOnstack lowlink sommet = match lowlink with
+	(som, index, link, onstack)::reste -> if som = sommet then onstack else getOnstack reste sommet
+	| [] -> failwith "Sommet Invalide getOnstack";;
+
+
+
+let min a b = if a < b then a else b;;
+
 
 
 (*retourne la liste des successeurs de sommet dans graphe*)
 let rec successeurs graphe sommet = match graphe with
 	((x, s)::r) -> if x = sommet then s else (successeurs r sommet)
 	| [] -> failwith "Ce sommet n'appartient pas au graphe";;
-(*successeurs graphe1 3;;*)
 
 
-(*vrai si e appartient a l*)
-let rec contient l e = match l with
-	(x::r) -> if x = e 	then true
-						else contient r e
-	| [] -> false;;
+(*c'est cette fonction qui fait la recursivite de la fonction tarjan et qui mets a jours les donnees au fur a musure*)
+(*val strongconnect :  ('a * 'a list) list ->  ('a * int * int * bool) list -> 'a -> 'a list -> ('a * int * int * bool) list = <fun> *)
+let rec strongconnect graphe lowlink sommet pile=
+	let newIndex = (unusedIndex lowlink) + 1 in
+	let lowlink = setIndex lowlink sommet newIndex in
+	let lowlink = setLink lowlink sommet newIndex in
+	let pile = sommet::pile in
+	let lowlink = setOnstack lowlink sommet true in
+
+	let succ = successeurs graphe sommet in
 
 
-(*retire les elements de l_i de l_ii*)
-let rec retirer l1 l2 = match l1 with
-	(x::r) -> if contient l2 x 	then retirer r l2
-									else x :: retirer r l2
-	| [] -> [];;
-(*retirer [1;2;3;4;5] [2;4];;*)
+	let rec forSucc succes low = match succes with
+		s::r -> if (getIndex low s) = -1 then let low = strongconnect graphe low s pile in
+			let low = setLink low s (min (getLink low s) (getLink low sommet)) in forSucc r low
+				else if (getOnstack low s) = true then let low = setLink low sommet (min (getLink low sommet) (getIndex low s)) in forSucc r low
+					else forSucc r low
+		| [] -> low
+
+	in let lowlink = forSucc succ lowlink in lowlink;;
 
 
-(*Pour chaque sommet de graphe, inverser les successeurs*)
-let rec inverserSuccesseurs graphe listeSommets =	match graphe with
-	(x, s)::r ->  (x, (retirer listeSommets s)) :: inverserSuccesseurs r listeSommets
-	| [] -> [];;
+(*cette fonction sert juste a formatter les composantes connexes pour etre comme dans l'enonce*)
+(*val postTraitement : ('a * 'b * int * 'c) list -> 'a list list = <fun>*)
+let postTraitement lowlink =
+
+	(*fonction qui enleve les informations inutiles*)
+	let rec clean low = match low with
+		(sommet, _, link, _)::reste -> (sommet, link+1) :: (clean reste)
+		| [] -> []
+
+	(*fonction qui determine le nombre de composantes connexes*)
+	in let rec nbgroupes links = match links with
+		(sommet, link)::reste -> let maxReste = nbgroupes reste in if maxReste > link then maxReste else link
+		| [] -> -1
+
+	(*fonction qui ajoute des composantes vides en fonction du nombre de composantes connexes*)
+	in let rec addgroup composantes maxgroupes = if maxgroupes = 0 then []::composantes
+										else []:: (addgroup composantes (maxgroupes-1))
+
+	(*maintenant les listes des composantes connexes sont pret a etre rempli*)
+	in let composantes = addgroup [] (nbgroupes (clean lowlink))
+
+	(*les deux fonctions suivantes sont ceux qui remplissent les listes des composantes connexes*)
+	in let rec addcomposante composantes sommet link = match composantes with
+		c::r -> if link = 0 then (sommet::c)::r
+				else c:: addcomposante r sommet (link-1)
+		| [] -> failwith "composante non existant"
+
+	in let rec cfc links composantes = match links with
+		(sommet, link)::r -> cfc r (addcomposante composantes sommet link)
+		| [] -> composantes
+
+	in cfc (clean lowlink) composantes;;
 
 
-(*inverse graphe*)
-let inverser graphe = inverserSuccesseurs graphe (liste_sommets graphe);;
-(*inverser graphe1;;*)
+
+(*fonction principale de tarjan, initialise les donnees necessaires*)
+(*val tarjan : ('a * 'a list) list -> 'a list list = <fun>  *)
+let tarjan graphe =
+	let lowlink = init_lowlink graphe in
+	let pile = [] in
+
+		let rec check low = match low with
+			(sommet, index, link, onstack)::reste -> if index = -1 then strongconnect graphe lowlink sommet pile
+													else (sommet, index, link, onstack):: check reste
+			| [] -> lowlink
+
+	in let lowlink = check lowlink in postTraitement lowlink;;
 
 
-(*
-let rec DFS graphe discovered racine = match graphe with
-	(x, (e::r))::g -> if x != racine
-*)
 
-
-
-(*
-let parcours_profondeur graphe =
-
-	let rec parcours_interne sommet visited notVisited =
-		if (notVisited = []) 	then []
-								else match sommet with
-								(num, succ) -> if contient notVisited num 	then num:: (parcours_interne )
-
-
-*)
+tarjan graphe1;;
