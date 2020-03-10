d0293c2d94575f8ebac9b7bc2fb7cf93185f4c58
BrinonBat <baptiste@brinon.eu>
2020-03-10 15:21:41 +0100

    TD3 prédicats finis, reste tris

--- S6/progLog/TD3/TD3.pl
+++ S6/progLog/TD3/TD3.pl
@@ -11,14 +11,14 @@ place(X,[Y|T],[X,Y|T]):-inf_st(X,Y).
 place(X,[X|T],[X,X|T]).
 place(X,[Y|T],[Y|TX]):-inf_st(Y,X),place(X,T,TX).
 
-dpeda(_P[],[],[],[]).
-dpeda(P,[X|L],PPP,[X[EP],PGP):-P=X,dpeda(P,L,PPP,EP,PGD).
-dpeda(P,[X|L],PPP,EP,[X|PGP]):-inf_st(P,X),dpeda(P,L,PPP,EP,PGD).
-dpeda(P,[X|L],[X|PPP],EP,PGP):-inf_st(X,P),dpeda(P,L,PPP,EP,PGD).
+dpeda(_P,[],[],[],[]).
+dpeda(P,[X|L],PPP,[X|EP],PGP):-P=X,dpeda(P,L,PPP,EP,PGP).
+dpeda(P,[X|L],PPP,EP,[X|PGP]):-inf_st(P,X),dpeda(P,L,PPP,EP,PGP).
+dpeda(P,[X|L],[X|PPP],EP,PGP):-inf_st(X,P),dpeda(P,L,PPP,EP,PGP).
 
-fusion([],[],[]).
+%fusion([],[],[]).
 fusion(T,[],T).
 fusion([],T,T).
-fusion([X|T],[Y|T_],[X|TT_]):-inf_st(X,Y),fusion(T,[Y,T_],TT_).
+fusion([X|T],[Y|T_],[X|TT_]):-inf_st(X,Y),fusion(T,[Y|T_],TT_).
 fusion([X|T],[Y|T_],[Y|TT_]):-inf_st(Y,X),fusion([X|T],T_,TT_).
 fusion([X|T],[X|T_],[X,X|TT_]):-fusion(T,T_,TT_).
